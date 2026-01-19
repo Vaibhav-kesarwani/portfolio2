9a2b153c83ab8670edb542f1ee5052303cd48065
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2026-01-20 00:10:46 +0530

    chore: change the font to the cabinet font

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
 export default function Home() {
-  return <div>Hello Vaibhav Kesarwani</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <span
+        style={{
+          fontFamily: "var(--font-cabinet)",
+          fontSize: "200px",
+          fontWeight: 900,
+        }}
+        className="leading-none"
+      >
+        Vaibhav
+      </span>
+    </div>
+  );
 }
