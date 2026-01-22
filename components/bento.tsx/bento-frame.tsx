76f5fe813f6a8e5dd4d27b2b508bda47785f67af
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2026-01-22 14:04:35 +0530

    chore: make the bento overlay card

--- components/bento.tsx/bento-frame.tsx
+++ components/bento.tsx/bento-frame.tsx
@@ -28,8 +28,21 @@ export default function BentoFrame() {
         <ContactCard />
       </div>
 
-      <div className="border border-red-500 col-span-2 rounded-3xl"></div>
-      <div className="border border-red-500 col-span-2 rounded-3xl"></div>
+      <div className="col-span-2 rounded-3xl border border-white/10"></div>
+      <div className="col-span-2 rounded-3xl border border-white/10"></div>
+
+      <div className="pointer-events-none absolute top-[73.4%] left-1/2 z-10 -translate-x-1/2 -translate-y-1/2">
+        <div className="relative size-100 rounded-full shadow-[0_0_100px_rgba(255,255,255,0.01)] backdrop-blur-xl">
+          <div className="absolute inset-0 grid grid-rows-2 gap-6.5">
+            <div className="rounded-t-full border border-white/10 border-b-0" />
+
+            <div className="grid grid-cols-2 gap-6.5">
+              <div className="border-l border-b border-white/10 rounded-bl-full" />
+              <div className="border-r border-b border-white/10 rounded-br-full" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
