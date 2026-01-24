cbcc8f36a94a51ee59a34fbb4a8090541f807f56
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2026-01-24 16:26:10 +0530

    chore: make the work route

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import Project from "@/components/projects/project";
+import GithubStats from "@/components/vaibhav-ui/github-stats";
+import SectionMainText from "@/components/vaibhav-ui/section-main-text";
 
 export default function page() {
   return (
-    <div>page</div>
-  )
+    <section className="w-full flex flex-col items-center">
+      <div className="min-h-screen w-full flex items-center justify-center px-6">
+        <SectionMainText
+          mainText="My Works"
+          secondText="Crafting digital experiences"
+          cursiveText="with passion & code."
+        />
+      </div>
+
+      <div className="w-full flex justify-center px-6">
+        <Project />
+      </div>
+
+      <GithubStats />
+    </section>
+  );
 }
