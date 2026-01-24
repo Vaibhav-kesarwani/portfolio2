fd8065b9cc81d4d4554cc1cbe605c2be73b2b1f7
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2026-01-24 14:52:43 +0530

    chore: make the project section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/about/about";
 import BentoFrame from "@/components/bento/bento-frame";
 import HeroText from "@/components/hero/hero-text";
+import Project from "@/components/projects/project";
 import Skill from "@/components/skill/skill";
 import RibbonMarquee from "@/components/vaibhav-ui/ribbon-marquee";
 
@@ -9,6 +10,7 @@ export default function Home() {
     <>
       <HeroText />
       <BentoFrame />
+      <Project />
       <Skill />
       <RibbonMarquee />
       <About />
