b211f69dc633a15e38913525245943bcd4fbc045
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2026-01-28 19:14:11 +0530

    chore: init the portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,3 @@
-import About from "@/components/about/about";
-import BentoFrame from "@/components/bento/bento-frame";
-import HeroText from "@/components/hero/hero-text";
-import Project from "@/components/projects/project";
-import Skill from "@/components/skill/skill";
-import RibbonMarquee from "@/components/vaibhav-ui/ribbon-marquee";
-
 export default function Home() {
-  return (
-    <>
-      <HeroText />
-      <BentoFrame />
-      <Project />
-      <Skill />
-      <RibbonMarquee />
-      <About />
-    </>
-  );
+  return <div>Hello World</div>;
 }
