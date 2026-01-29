90b613acef103d9441ab818644f2ae518f0e34b2
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2026-01-29 19:55:42 +0530

    chore: make the about and github heatmap

---
+++ config/about.tsx
@@ -0,0 +1,25 @@
+import JavaScript from "@/components/technologies/JavaScript";
+import MongoDB from "@/components/technologies/MongoDB";
+import NextJs from "@/components/technologies/NextJs";
+import NodeJs from "@/components/technologies/NodeJs";
+import PostgreSQL from "@/components/technologies/PostgreSQL";
+import Prisma from "@/components/technologies/Prisma";
+import ReactIcon from "@/components/technologies/ReactIcon";
+import TypeScript from "@/components/technologies/TypeScript";
+
+export const mySkills = [
+  <NextJs key="nextjs" />,
+  <TypeScript key="typescript" />,
+  <ReactIcon key="react" />,
+  <JavaScript key="javascript" />,
+  <MongoDB key="mongodb" />,
+  <NodeJs key="nodejs" />,
+  <PostgreSQL key="postgresql" />,
+  <Prisma key="prisma" />,
+];
+
+export const about = {
+  name: "Vaibhav Kesarwani",
+  description:
+    "I'm a Full Stack web developer and Open Source Contributor passionate about building products that solve real world problems. I specialize in crafting and launching MVPs that move quickly from idea to production.",
+};
