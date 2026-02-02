858c2d7d4a55f336cb58d2f5a4e762114f74d19a
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2026-02-02 15:51:09 +0530

    add the fumadocs components

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -3,7 +3,9 @@ import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: "Welcome to Vaibhav Lab's",
+      title: "Vaibhav Lab's",
+      transparentMode: 'top',
     },
+    githubUrl: "https://github.com/Vaibhav-kesarwani",
   };
 }
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,36 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx';
-import type { MDXComponents } from 'mdx/types';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as Twoslash from "fumadocs-twoslash/ui";
+import {
+  createFileSystemGeneratorCache,
+  createGenerator,
+} from "fumadocs-typescript";
+import { AutoTypeTable } from "fumadocs-typescript/ui";
+import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
+import { ImageZoom } from "fumadocs-ui/components/image-zoom";
+import * as TabsComponents from "fumadocs-ui/components/tabs";
+import defaultComponents from "fumadocs-ui/mdx";
+import type { MDXComponents } from "mdx/types";
+
+const generator = createGenerator({
+  // set a cache, necessary for serverless platform like Vercel
+  cache: createFileSystemGeneratorCache(".next/fumadocs-typescript"),
+});
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
-    ...defaultMdxComponents,
+    ...defaultComponents,
+    ...Twoslash,
+    ...TabsComponents,
+    AutoTypeTable: (props) => (
+      <AutoTypeTable {...props} generator={generator} />
+    ),
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    pre: ({ ref: _ref, ...props }) => (
+      <CodeBlock {...props}>
+        <Pre>{props.children}</Pre>
+      </CodeBlock>
+    ),
+    img: (props) => <ImageZoom {...(props as any)} />,
     ...components,
   };
 }
