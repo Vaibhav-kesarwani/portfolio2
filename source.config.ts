2035ab5914ecc13d771b077651a8d96391212c5b
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2026-02-02 17:50:40 +0530

    feat: make the fumadocs llm working and also add the latest page view

--- source.config.ts
+++ source.config.ts
@@ -1,12 +1,14 @@
 import { rehypeCodeDefaultOptions } from "fumadocs-core/mdx-plugins";
 import { defineConfig, defineDocs } from "fumadocs-mdx/config";
-import { transformerTwoslash } from 'fumadocs-twoslash';
+import lastModified from "fumadocs-mdx/plugins/last-modified";
+import { transformerTwoslash } from "fumadocs-twoslash";
 
 export const docs = defineDocs({
   dir: "content/docs",
 });
 
 export default defineConfig({
+  plugins: [lastModified()],
   mdxOptions: {
     remarkCodeTabOptions: {
       parseMdx: true,
