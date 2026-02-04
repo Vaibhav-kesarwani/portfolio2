0c9294a71f6a61937362175f6af41edb0b6676ef
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2026-02-04 14:36:09 +0530

    chore: add more quotes in the config

--- config/quote.tsx
+++ config/quote.tsx
@@ -21,7 +21,72 @@ export const quotes = [
     author: 'Sung Jin-Woo, Solo Leveling',
   },
   {
-    quote: `I’ll take a potato chip… AND EAT IT!`,
+    quote: `I'll take a potato chip… AND EAT IT!`,
     author: 'Light Yagami, Death Note',
+  },
+
+  {
+    quote: `The man who moves a mountain begins by carrying away small stones.`,
+    author: 'Confucius',
+  },
+  {
+    quote: `Discipline is choosing what you want most over what you want now.`,
+    author: 'Abraham Lincoln',
+  },
+  {
+    quote: `You don't rise to the level of your goals, you fall to the level of your systems.`,
+    author: 'James Clear',
+  },
+  {
+    quote: `He who has a why to live can bear almost any how.`,
+    author: 'Friedrich Nietzsche',
+  },
+  {
+    quote: `Do not pray for an easy life. Pray for the strength to endure a difficult one.`,
+    author: 'Bruce Lee',
+  },
+  {
+    quote: `Hard work beats talent when talent doesn't work hard.`,
+    author: 'Tim Notke',
+  },
+  {
+    quote: `Push yourself, because no one else is going to do it for you.`,
+    author: 'Unknown',
+  },
+  {
+    quote: `Great things never come from comfort zones.`,
+    author: 'Unknown',
+  },
+  {
+    quote: `Suffer the pain of discipline or suffer the pain of regret.`,
+    author: 'Jim Rohn',
+  },
+  {
+    quote: `Your future is created by what you do today, not tomorrow.`,
+    author: 'Robert Kiyosaki',
+  },
+  {
+    quote: `I am the hope of the universe.`,
+    author: 'Goku, Dragon Ball Z',
+  },
+  {
+    quote: `Whatever you lose, you'll find it again.`,
+    author: 'Naruto Uzumaki, Naruto',
+  },
+  {
+    quote: `If you don't take risks, you can't create a future.`,
+    author: 'Monkey D. Luffy, One Piece',
+  },
+  {
+    quote: `The only way to prove you are strong is to fight.`,
+    author: 'Ichigo Kurosaki, Bleach',
+  },
+  {
+    quote: `We don't know what tomorrow holds. That's why we can live for today.`,
+    author: 'Natsu Dragneel, Fairy Tail',
+  },
+  {
+    quote: `No matter how deep the night, it always turns to day.`,
+    author: 'Brook, One Piece',
   },
 ];
